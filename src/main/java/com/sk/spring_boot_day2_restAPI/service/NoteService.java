5b08dec33fed0a90f34507ace6fdcee083fe1c37
Santhosh Kumar R <learnwithsanthosh@gmail.com>
2025-06-23 18:12:27 +0530

    chore: Added logging and basic unit tests for notes service (Day 6)

--- src/main/java/com/sk/spring_boot_day2_restAPI/service/NoteService.java
+++ src/main/java/com/sk/spring_boot_day2_restAPI/service/NoteService.java
@@ -1,8 +1,12 @@
 package com.sk.spring_boot_day2_restAPI.service;
 
+import com.sk.spring_boot_day2_restAPI.controller.NoteController;
 import com.sk.spring_boot_day2_restAPI.dao.Notes;
 import com.sk.spring_boot_day2_restAPI.exception.NotesNotFoundException;
 import com.sk.spring_boot_day2_restAPI.repo.NotesRepo;
+import org.aspectj.weaver.ast.Not;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +14,7 @@ import java.util.Optional;
 
 @Service
 public class NoteService {
+    private static final Logger logger = LoggerFactory.getLogger(NoteController.class);
 
     private NotesRepo notesRepo;
 
@@ -19,18 +24,27 @@ public class NoteService {
 
 
     public List<Notes> getallNotes() {
-            return notesRepo.findAll();
+        logger.info(" Inside NoteService.getallNotes()");
+        return notesRepo.findAll();
     }
 
     public void addNote(Notes note) {
+        logger.info(" Inside NoteService.addNote() :{}",note);
             notesRepo.save(note);
     }
 
     public Notes getNoteById(int id) {
-        return notesRepo.findById(id).orElseThrow(()->new NotesNotFoundException("Note not found for given id" +id));
+        logger.info("Inside NoteService.getNoteById() with id : {}",id);
+        Notes note= notesRepo.findById(id).get();
+        if (note.equals(null)) {
+            logger.error("Note not found for given id: "+id);
+            throw new NotesNotFoundException("Note not found for given id :"+id);
+        }
+        return note;
     }
 
     public String deleteById(int id) {
+        logger.info("Inside NoteService.deleteById() with id : {}",id);
         notesRepo.deleteById(id);
         return "Note with id :"+id+" deleted";
     }
---
+++ src/test/java/com/sk/spring_boot_day2_restAPI/service/NoteServiceTest.java
@@ -0,0 +1,53 @@
+package com.sk.spring_boot_day2_restAPI.service;
+
+import com.sk.spring_boot_day2_restAPI.controller.NoteController;
+import com.sk.spring_boot_day2_restAPI.dao.Notes;
+import com.sk.spring_boot_day2_restAPI.repo.NotesRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+public class NoteServiceTest {
+
+    @Mock
+    private NoteController noteController;
+
+    @Mock
+    private NotesRepo notesRepo;
+
+    @InjectMocks
+    private NoteService noteService;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetallNotes(){
+        when(notesRepo.findAll()).thenReturn(new ArrayList<>());
+        var response=noteService.getallNotes();
+        assertNotNull(response);
+    }
+
+    @Test
+    public void testGetNoteById(){
+        Notes note = new Notes();
+        note.setId(52);
+        note.setName("Test Note");
+        note.setSubject("This is a test note.");
+        when(notesRepo.findById(anyInt())).thenReturn(Optional.of(note));
+        var response = noteService.getNoteById(52);
+        assertNotNull(response);
+    }
+}
